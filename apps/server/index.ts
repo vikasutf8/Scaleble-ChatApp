74333db3b593ad3cd0925e8ce8e2d04a9dcc5aba
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-04-15 22:52:40 +0530

    Fecture: listiner

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -13,6 +13,7 @@ async function startServer() {
 
     socketService.io.attach(server);
     
+    socketService.initListener();
 
     server.listen(8001);
     console.log('Server is running on port 8001');
--- apps/server/services/socket.ts
+++ apps/server/services/socket.ts
@@ -1,27 +1,44 @@
-import { Server } from 'socket.io';
-
+import { log } from "console";
+import { Server } from "socket.io";
 
 class SocketService {
-    private _io: Server;
-    constructor() {
-        console.log('Socket service is running');
-        this._io = new Server(8001);
-    }
-    get io() {
-        return this._io;
-    }
-    // start() {
-    //     this.io.on('connection', (socket) => {
-    //         console.log('A user connected');
+  private _io: Server;
+  constructor() {
+    console.log("Socket service is running");
+    this._io = new Server(8001);
+  }
+  get io() {
+    return this._io;
+  }
+  // start() {
+  //     this.io.on('connection', (socket) => {
+  //         console.log('A user connected');
 
-    //         socket.on('disconnect', () => {
-    //             console.log('A user disconnected');
-    //         });
-    //     });
+  //         socket.on('disconnect', () => {
+  //             console.log('A user disconnected');
+  //         });
+  //     });
 
-    //     this.io.listen(8001);
-    // }
+  //     this.io.listen(8001);
+  // }
+
+  public initListener() {
+    console.log("initListener");
+    
+    const io = this.io;
+    io.on("connect", async (socket) => {
+      console.log("A user connected", socket.id);
+
+      //when ever a new message is received/send from the client
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("new message received", message);
+        // await io.emit('event:message', message);
+      });
+
+      await socket.on("disconnect", () => {
+        console.log("A user disconnected");
+      });
+    });
+  }
 }
-
-
 export default SocketService;
