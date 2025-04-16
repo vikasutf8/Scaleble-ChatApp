13819404589de9b2d1eb70238560a554232796a2
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-04-16 23:25:00 +0530

    E:emit message

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,45 +1,56 @@
-'use client'
-
-
-import React, { useCallback, useContext, useEffect, createContext } from "react";
+"use client";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  createContext,
+  useState,
+} from "react";
 import { io, Socket } from "socket.io-client";
 
 interface SocketContextProps {
-    children?: React.ReactNode
+  children?: React.ReactNode;
 }
 interface ISocketContext {
-    socket: Socket | any;
-    sendMessage: (message: string) => any;
+  socket: Socket | any;
+  sendMessage: (message: string) => any;
 }
 export const SocketContext = createContext<ISocketContext | null>(null);
 
 // custom hook for using SocketContext
 export const useSocket = () => {
-    const state = useContext(SocketContext);
-    if (!state) {
-      throw new Error("useSocket must be used within a SocketProvider");
-    }
-    return state;
-  };
+  const state = useContext(SocketContext);
+  if (!state) {
+    throw new Error("useSocket must be used within a SocketProvider");
+  }
+  return state;
+};
 
 export const SocketProvider: React.FC<SocketContextProps> = ({ children }) => {
-    //now implement ISocketContext internal functions
-    const sendMessage: ISocketContext['sendMessage'] = useCallback((message: string) => {
-        console.log("sendMessage ISocketContext", message);
-    }, [])
+  const [socket, setSocket] = useState<Socket | any>(null);
 
-    //TODO : implement input field and send buttom on sendMessage ->that send the message to the server --using socket.io-client
-    useEffect(() => {
-        const _socket = io("http://localhost:8009"); //connect to the server
+  //now implement ISocketContext internal functions
+  const sendMessage: ISocketContext["sendMessage"] = useCallback(
+    (message: string) => {
+      console.log("sendMessage ISocketContext", message);
 
-        return () => {
-            _socket.disconnect();
-        }
-    })
-    return (
-        <SocketContext.Provider value={{ socket: Socket, sendMessage }}>
-            {children}
-        </SocketContext.Provider>
-    )
-}
+      socket.emit("event:message", { message: message });
+    },
+    
+  );
 
+  //TODO : implement input field and send buttom on sendMessage ->that send the message to the server --using socket.io-client
+  useEffect(() => {
+    const _socket = io("http://localhost:8009"); //connect to the server
+    setSocket(_socket);
+    return () => {
+      _socket.disconnect();
+      setSocket(undefined);
+    };
+  },[socket]);
+  return (
+    <SocketContext.Provider value={{ socket, sendMessage }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
